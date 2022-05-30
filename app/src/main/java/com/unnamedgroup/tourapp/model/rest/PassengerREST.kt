ace2021acd5c0aea1fa3ed4fe71403c541ddd6b3
benjamenriquez <benjamenriquez@gmail.com>
2022-05-30 20:01:43 -0300

    fix inverted dni and name in PassengerREST

--- app/src/main/java/com/unnamedgroup/tourapp/model/rest/PassengerREST.kt
+++ app/src/main/java/com/unnamedgroup/tourapp/model/rest/PassengerREST.kt
@@ -3,13 +3,13 @@ package com.unnamedgroup.tourapp.model.rest
 import com.unnamedgroup.tourapp.model.business.Passenger
 
 data class PassengerREST(
-    val dni: String,
     val name: String,
+    val dni: String,
 ) {
     fun toPassenger(): Passenger {
         return Passenger(
-            dni,
             name,
+            dni,
         )
     }
 }
