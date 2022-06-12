8e9392a69e3449271d245f32868af00f5583d220
benjamenriquez <benjamenriquez@gmail.com>
2022-06-12 20:41:04 -0300

    fix no defined var

--- app/src/driver/java/com.unnamedgroup.tourapp/view/fragment/QrScannerFragment.kt
+++ app/src/driver/java/com.unnamedgroup.tourapp/view/fragment/QrScannerFragment.kt
@@ -60,6 +60,8 @@ class QrScannerFragment : Fragment() {
                 }
                 else {
                     Toast.makeText(context, getString(R.string.must_grant_permission), Toast.LENGTH_SHORT).show()
+                    val bundle = Bundle()
+                    bundle.putParcelable("Trip", trip)
                     findNavController().navigate(R.id.action_QrScannerFragment_to_TripDetailsDriverFragment, bundle)
                 }
             }
